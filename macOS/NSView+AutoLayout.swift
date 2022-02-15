1ab743e3736205e5a5f0e5fd117d073c02384557
IKEH1024 <pondau1024@gmail.com>
2022-02-15 19:40:17 +0900

    add NSView+AutoLayout

---
+++ macOS/NSView+AutoLayout.swift
@@ -0,0 +1,102 @@
+//
+//  NSView+AutoLayout.swift
+//  QuickImageUploader
+//
+//  Created by HIROKI IKEUCHI on 2022/02/15.
+//
+
+import AppKit
+
+// 全てのUIパーツはNSViewを継承しているので、NSViewのExtensionとしてConstrainsのためのメソッドを用意する
+extension NSView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+
+        translatesAutoresizingMaskIntoConstraints = false
+
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+
+    func center(inView view: NSView, yConstant: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: yConstant).isActive = true
+    }
+
+    func centerX(inView view: NSView, topAnchor: NSLayoutYAxisAnchor? = nil, paddingTop: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        if let topAnchor = topAnchor {
+            self.topAnchor.constraint(equalTo: topAnchor, constant: paddingTop).isActive = true
+        }
+    }
+
+    func centerY(inView view: NSView,
+                 leftAnchor: NSLayoutXAxisAnchor? = nil,
+                 paddingLeft: CGFloat? = nil,
+                 constant: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+
+        if let leftAnchor = leftAnchor,
+           let padding = paddingLeft {
+            self.leftAnchor.constraint(equalTo: leftAnchor, constant: padding).isActive = true
+        }
+    }
+
+    func setDimensions(width: CGFloat? = nil, height: CGFloat? = nil) {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+
+    func setSizeAspect(widthRatio: CGFloat = 1.0, heightRatio: CGFloat = 1.0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalTo: widthAnchor, multiplier: heightRatio / widthRatio).isActive = true
+    }
+
+    func addConstraintsToFillView(_ view: NSView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        anchor(top: view.topAnchor,
+               left: view.leftAnchor,
+               right: view.rightAnchor,
+               bottom: view.bottomAnchor)
+    }
+}
