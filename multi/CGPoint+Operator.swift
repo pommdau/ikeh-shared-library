64fc0153508d47c2b291381cc6bce22b9a8ef79b
IKEH1024 <pondau1024@gmail.com>
2023-12-19 15:09:43 +0900

    append resources

---
+++ multi/CGPoint+Operator.swift
@@ -0,0 +1,60 @@
+//
+//  CGPoint+Operator.swift
+//  GlobalMouseEventDemo
+//
+//  Created by HIROKI IKEUCHI on 2023/07/23.
+//
+
+import Foundation
+
+// MARK: - CGPoint
+
+extension CGPoint {
+    static func + (l: CGPoint, r: CGPoint) -> CGPoint {
+        CGPoint(x: l.x + r.x, y: l.y + r.y)
+    }
+    
+    static func - (l: CGPoint, r: CGPoint) -> CGPoint {
+        CGPoint(x: l.x - r.x, y: l.y - r.y)
+    }
+    
+    static func * (l: CGFloat, r: CGPoint) -> CGPoint {
+        CGPoint(x: l * r.x, y: l * r.y)
+    }
+    
+    static func / (l: CGPoint, r: CGFloat) -> CGPoint {
+        if r == 0.0 {
+            return CGPoint.zero
+        }
+        return CGPoint(x: l.x / r, y: l.y / r)
+    }
+    
+    static func += (lhs: inout CGPoint, rhs: CGPoint) {
+        lhs.x += rhs.x
+        lhs.y += rhs.y
+    }
+}
+
+// MARK: - CGRect
+
+extension CGRect {
+    static func * (l: CGFloat, r: CGRect) -> CGRect {
+        CGRect(x: l * r.origin.x, y: l * r.origin.y, width: l * r.width, height: l * r.height)
+    }
+}
+
+// MARK: - CGSize
+
+extension CGSize {
+    static func + (l: CGSize, r: CGSize) -> CGSize {
+        CGSize(width: l.width + r.width, height: l.height + r.height)
+    }
+    
+    static func * (l: CGFloat, r: CGSize) -> CGSize {
+        CGSize(width: l * r.width, height: l * r.height)
+    }
+    
+    static func += (lhs: inout CGSize, rhs: CGSize) {
+        lhs = .init(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+}
